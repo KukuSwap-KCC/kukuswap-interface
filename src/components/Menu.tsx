6877083d6debac845576f50ba0eee34c39e1901c
kukuswap-team <kukuswap@gmail.com>
2021-07-14 18:35:50 +0300

    feat: final version

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,29 +10,14 @@ import { useLingui } from '@lingui/react'
 
 const items = (i18n: I18n) => [
     {
-        name: i18n._(t`Docs`),
-        description: i18n._(t`Documentation for users of Sushi.`),
-        href: 'https://docs.sushi.com'
+        name: i18n._(t`Twitter`),
+        description: i18n._(t`http://twitter.com/kukuswapio`),
+        href: 'http://twitter.com/kukuswapio'
     },
     {
-        name: i18n._(t`Dev`),
-        description: i18n._(t`Documentation for developers of Sushi.`),
-        href: 'https://dev.sushi.com'
-    },
-    {
-        name: i18n._(t`Open Source`),
-        description: i18n._(t`Sushi is a supporter of Open Source.`),
-        href: 'https://github.com/sushiswap'
-    },
-    {
-        name: i18n._(t`Tools`),
-        description: i18n._(t`Tools to optimize your workflow.`),
-        href: '/tools'
-    },
-    {
-        name: i18n._(t`Discord`),
-        description: i18n._(t`Join the community on Discord.`),
-        href: 'https://discord.gg/NVPXN4e'
+        name: i18n._(t`Telegram`),
+        description: i18n._(`https://t.me/KuKuSwapio`),
+        href: 'https://t.me/KuKuSwapio'
     }
 ]
 
@@ -42,6 +27,46 @@ export default function Menu() {
 
     return (
         <Popover className="relative">
+            {({ open }) => (
+                <>
+                    <Popover.Button
+                        className={classNames(open ? 'text-secondary' : 'text-primary', 'focus:outline-none')}
+                    >
+                    Socials
+                    </Popover.Button>
+
+                    <Transition
+                        show={open}
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1"
+                    >
+                        <Popover.Panel
+                            static
+                            className="absolute z-10 bottom-12 lg:top-12 left-full transform -translate-x-full mt-3 px-2 w-screen max-w-xs sm:px-0"
+                        >
+                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                <div className="relative grid gap-6 bg-dark-900 px-5 py-6 sm:gap-8 sm:p-8">
+                                    {solutions.map(item => (
+                                        <ExternalLink
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-m-3 p-3 block rounded-md hover:bg-dark-800 transition ease-in-out duration-150"
+                                        >
+                                            <p className="text-base font-medium text-high-emphesis">{item.name}</p>
+                                            <p className="mt-1 text-sm text-secondary">{item.description}</p>
+                                        </ExternalLink>
+                                    ))}
+                                </div>
+                            </div>
+                        </Popover.Panel>
+                    </Transition>
+                </>
+            )}
         </Popover>
     )
 }
